99728c7081e1cc3ca599ff7ee740697b661dbfc3
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2015-12-08 19:20:47 +0100

    added solution for day 5 part 2 (and refactored existing code)

--- src/main/java/net/kenvanhoeylandt/solutions/day5/Day5Solution.java
+++ src/main/java/net/kenvanhoeylandt/solutions/day5/Day5Solution.java
@@ -14,24 +14,32 @@ public class Day5Solution extends Solution
 	{
 		String[] lines = input.split("\n");
 
+		int part_one = solve(lines, line ->
+			line.matches(".*[aeiou].*[aeiou].*[aeiou].*")
+			&& line.matches(".*([a-z])\\1.*")
+			&& !line.matches(".*(ab|cd|pq|xy).*")
+		);
+
+		int part_two = solve(lines, line ->
+			line.matches(".*([a-z][a-z]).*\\1.*")
+			&& line.matches(".*([a-z])[a-z]\\1.*")
+		);
+
+		return String.format("part one: %d, part two: %d", part_one, part_two);
+	}
+
+	private int solve(String[] lines, Matcher matcher)
+	{
 		int count = 0;
 
 		for (String line : lines)
 		{
-			if (isMatch(line))
+			if (matcher.isMatch(line))
 			{
-				System.out.println("match: " + line);
 				count++;
 			}
 		}
 
 		return count;
-	}
-
-	boolean isMatch(String line)
-	{
-		return line.matches(".*[aeiou].*[aeiou].*[aeiou].*")
-			&& line.matches(".*([a-z])\\1.*")
-			&& !line.matches(".*(ab|cd|pq|xy).*");
 	}
 }
---
+++ src/main/java/net/kenvanhoeylandt/solutions/day5/Matcher.java
@@ -0,0 +1,6 @@
+package net.kenvanhoeylandt.solutions.day5;
+
+public interface Matcher
+{
+	boolean isMatch(String line);
+}
