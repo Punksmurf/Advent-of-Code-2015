8145b402ec1a0386cd79749c1a46d07ddbca1558
Niels Keetlaer <niels@elastique.nl>
2015-12-09 12:02:53 +0100

    Remove Ken's solutions Add my solution for day 1

--- src/main/java/net/kenvanhoeylandt/solutions/day1/Day1Solution.java
+++ src/main/java/net/kenvanhoeylandt/solutions/day1/Day1Solution.java
@@ -14,42 +14,28 @@ public class Day1Solution extends net.kenvanhoeylandt.solutions.Solution
 	protected Task<Object> solve(String input) throws Exception
 	{
 		int floor_index = 0;
-		int basement_counter = -1;
+        int basement_position = -1;
 
 		for (int i = 0; i < input.length(); ++i)
 		{
 			char character = input.charAt(i);
+            if (character == '(')
+            {
+                floor_index++;
+            }
+            else
+            {
+                floor_index--;
+            }
 
-			floor_index += getFloorDifference(character);
-
-			if (floor_index == -1 && basement_counter == -1)
-			{
-				basement_counter = i + 1;
-			}
+            if (floor_index < 0 && basement_position == -1)
+            {
+                basement_position = i + 1;
+            }
 		}
 
-		String result = String.format("Floors: %d, Basement counter: %d", floor_index, basement_counter);
+		String result = String.format("Floors: %d, Basement counter: %d", floor_index, basement_position);
 
 		return Task.forResult(result);
-	}
-
-	/**
-	 * @param character '(' means the floor goes up and ')' means the floor goes down. Anything else throws an exception
-	 * @return 1 when the floor goes up and -1 when it goes down
-	 * @throws InputParsingException on invalid input
-	 */
-	private int getFloorDifference(char character) throws InputParsingException
-	{
-		switch (character)
-		{
-			case '(':
-				return 1;
-
-			case ')':
-				return -1;
-
-			default:
-				throw new InputParsingException("Invalid input: \'" + character + "\'");
-		}
 	}
 }
--- src/main/java/net/kenvanhoeylandt/solutions/day3/data/Direction.java
+++
@@ -1,9 +0,0 @@
-package net.kenvanhoeylandt.solutions.day3.data;
-
-public enum Direction
-{
-	NORTH,
-	EAST,
-	SOUTH,
-	WEST
-}
